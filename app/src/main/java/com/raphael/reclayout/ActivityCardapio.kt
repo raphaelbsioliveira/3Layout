fcf4ba408ff815e66d51641a08e9f848e7f5feb0
ene-solucoes <raphaell.26@hotmail.com>
2021-01-19 23:05:01 -0300

    projeto 3 Layout

---
+++ app/src/main/java/com/raphael/reclayout/ActivityCardapio.kt
@@ -0,0 +1,11 @@
+package com.raphael.reclayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityCardapio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cardapio)
+    }
+}
---
+++ app/src/main/java/com/raphael/reclayout/ActivityTeclado.kt
@@ -0,0 +1,11 @@
+package com.raphael.reclayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityTeclado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teclado)
+    }
+}
